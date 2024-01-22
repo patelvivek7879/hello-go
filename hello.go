f4d2292e9505af824f7b11b450a331361875bf16
Vivek Patel <patelvivek7879@gmail.com>
2024-01-22 17:52:32 +0530

    hello-go gobyexample.com 1

--- hello.go
+++ hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main(){
-	fmt.Println("Hello, World.")
+	fmt.Println(ReverseRunes("vivek"))
+}
+
+func ReverseRunes(s string) string {
+	r := []rune(s)
+	for i := 0; i< 5; i i+1{
+		fmt.Println(i)
+	}
+return string(r)
 }
